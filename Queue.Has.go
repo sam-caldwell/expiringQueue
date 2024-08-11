64091b0715b7b4b1e6d7391cfa6991effd67c8c3
Sam Caldwell <github@samcaldwell.net>
2024-08-10 23:41:34 -0500

    Add Queue.Has() and test

--- Queue.Has.go
+++ Queue.Has.go
@@ -1,9 +1,13 @@
 package expiringQueue
 
-// Has - search the queue for a given element.
+// Has checks if a given value exists in the queue.
 func (q *Queue[T]) Has(value T) bool {
 	q.lock.Lock()
 	defer q.lock.Unlock()
+
+	if q.equalsFunc == nil {
+		panic("nil equalsFunc pointer in expiringQueue Has() method")
+	}
 
 	for node := q.head; node != nil; node = node.next {
 		if q.equalsFunc(node.value, value) {
@@ -12,5 +16,4 @@ func (q *Queue[T]) Has(value T) bool {
 	}
 
 	return false
-
 }
---
+++ Queue.Has_test.go
@@ -0,0 +1,41 @@
+package expiringQueue
+
+import (
+	"testing"
+	"time"
+)
+
+func equalsInt(a, b int) bool {
+	return a == b
+}
+
+func TestQueue_Has(t *testing.T) {
+	q := NewQueue[int](10*time.Second, equalsInt)
+	q.equalsFunc = equalsInt
+
+	// Test with an empty queue
+	if q.Has(10) {
+		t.Errorf("Expected value 10 to be absent in the empty queue")
+	}
+
+	// Push some values into the queue
+	q.Push(10)
+	q.Push(20)
+	q.Push(30)
+
+	// Test existing values
+	if !q.Has(10) {
+		t.Errorf("Expected value 10 to be found in the queue")
+	}
+	if !q.Has(20) {
+		t.Errorf("Expected value 20 to be found in the queue")
+	}
+	if !q.Has(30) {
+		t.Errorf("Expected value 30 to be found in the queue")
+	}
+
+	// Test non-existing value
+	if q.Has(40) {
+		t.Errorf("Expected value 40 to be absent in the queue")
+	}
+}
