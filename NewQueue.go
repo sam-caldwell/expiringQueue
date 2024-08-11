defdca0f3635e4c24d5dc819acafb0b6d49b5d1f
Sam Caldwell <github@samcaldwell.net>
2024-08-10 23:06:26 -0500

    fix up expiration parameters

--- NewQueue.go
+++ NewQueue.go
@@ -5,6 +5,7 @@ import "time"
 // NewQueue - Create and configure the queue.
 func NewQueue[T any](expiration time.Duration, equalsFunc func(a, b T) bool) *Queue[T] {
 	q := &Queue[T]{
+		expiration: expiration.Nanoseconds(),
 		equalsFunc: equalsFunc,
 	}
 	return q
--- Queue.ExpireNodes.go
+++ Queue.ExpireNodes.go
@@ -3,12 +3,18 @@ package expiringQueue
 import "time"
 
 // expireNodes - run in the background to expire nodes by age.
-func (q *Queue[T]) expireNodes(expiration int64) {
-	q.expiration = expiration
+func (q *Queue[T]) expireNodes() {
+	if q.expiration == 0 {
+		return
+	}
 	for {
 		var prev *Node[T]
 		time.Sleep(time.Duration(q.expiration) * time.Nanosecond)
 		q.lock.Lock()
+		if q.pauseExpirationTask {
+			q.lock.Unlock()
+			return
+		}
 		now := time.Now().UnixNano()
 		for node := q.head; node != nil; node = node.next {
 			if age := node.timestamp - now; age > q.expiration && q.deleteNode(node, prev) {
